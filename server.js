9c2e3c54bf55d08d62c102f6d429fb9df1d9b236
sevaggap <sevag.gaprielian@gmail.com>
2022-03-07 11:05:02 -0500

    added helper files

--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 const app = express();
 const PORT = process.env.PORT || 3001;
