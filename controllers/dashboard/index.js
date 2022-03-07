99e80b58ce0e0679d321f87d4117f5f6e2ecdf30
sevaggap <sevag.gaprielian@gmail.com>
2022-03-07 12:30:42 -0500

    redirect users to login page for edting a recipe

--- controllers/dashboard/index.js
+++ controllers/dashboard/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const addrecipeRoutes = require('./addrecipeRoutes')
+const editrecipeRoutes = require('./editrecipeRoutes')
 
 router.use('/new', addrecipeRoutes);
+router.use('/edit', editrecipeRoutes);
 
 module.exports = router;
